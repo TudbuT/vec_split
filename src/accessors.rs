150b5b6952ebdc62b9ac6f55bf15ecbe6f5327cd
TudbuT <tudbut@tudbut.de>
2023-05-18 05:50:51 +0200

    allow implementing Accessors for other types more easily

--- src/accessors.rs
+++ src/accessors.rs
@@ -5,6 +5,34 @@ use std::{
 
 use crate::iter::*;
 
+#[macro_export]
+macro_rules! impl_accessors {
+    ($type:ty, $getter:ty, unknown $index_constraints:tt) => {
+        impl<'a, T, I: $index_constraints> Accessor<T, I> for $type {
+            fn get<'b>(&'b self, index: I) -> Option<&'b T> {
+                <$getter>::get(self, index)
+            }
+        }
+        impl<'a, T, I: $index_constraints> AccessorMut<T, I> for $type {
+            fn get_mut<'b>(&'b mut self, index: I) -> Option<&'b mut T> {
+                <$getter>::get_mut(self, index)
+            }
+        }
+    };
+    ($type:ty, $getter:ty, known $index:ty) => {
+        impl<'a, T> Accessor<T, $index> for $type {
+            fn get<'b>(&'b self, index: $index) -> Option<&'b T> {
+                <$getter>::get(self, index)
+            }
+        }
+        impl<'a, T> AccessorMut<T, $index> for $type {
+            fn get_mut<'b>(&'b mut self, index: $index) -> Option<&'b mut T> {
+                <$getter>::get_mut(self, index)
+            }
+        }
+    };
+}
+
 pub trait Accessor<T: ?Sized, I>: Index<I, Output = T> {
     fn get<'b>(&'b self, index: I) -> Option<&'b T>;
 }
@@ -33,3 +61,6 @@ pub trait IterateAccessorMut<'a, T: ?Sized>: AccessorMut<T, usize> + 'a + Sized 
 
 impl<'a, T, A: Accessor<T, usize> + 'a> IterateAccessor<'a, T> for A {}
 impl<'a, T, A: AccessorMut<T, usize> + 'a> IterateAccessorMut<'a, T> for A {}
+
+impl_accessors!(Vec<T>, [T], known usize);
+impl_accessors!([T], [T], known usize);
